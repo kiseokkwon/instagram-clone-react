3a5686826d7deb62b6c55d0925b1d45a95e0ac65
kisskwon <kisskwon87@gmail.com>
2021-07-05 00:44:08 +0900

    implement comments and connect firebase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { auth, db } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Button, Input } from '@material-ui/core';
+import ImageUpload from './ImageUpload';
+import InstagramEmbed from 'react-instagram-embed';
 
 function getModalStyle() {
   const top = 50;
@@ -27,13 +29,13 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2, 4, 3),
   },
 }));
-
 function App() {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
 
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
@@ -43,7 +45,6 @@ function App() {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         // user has logged in...
-        console.log(authUser);
         setUser(authUser);
       } else {
         // user has logged out...
@@ -57,10 +58,9 @@ function App() {
     }
   }, [user, username]);
 
-
   useEffect(() => {
     // this is where the code runs
-    db.collection('posts').onSnapshot(snapshot => {
+    db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
       // every time a new post it added, this code fires...
       setPosts(snapshot.docs.map(doc => ({
         id: doc.id,
@@ -73,14 +73,49 @@ function App() {
     event.preventDefault();
 
     auth
-    .createUserWithEmailAndPassword(email, password)
-    .then((authUser) => {
-      return authUser.user.updateProfile({
-        displayName: username
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username
+        }).then(() => setUser(authUser));
       })
-    })
-    .catch((error) => alert(error.message));
-  }
+      .catch((error) => {
+        alert(error.message);
+        clearAll();
+      });
+
+    setOpen(false);
+  };
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        alert(error.message);
+        clearAll();
+      });
+
+    setOpenSignIn(false);
+  };
+
+  const signOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        console.log("sign out");
+        alert('You have been logged out successfully');
+        clearAll();
+      });
+  };
+
+  const clearAll = () => {
+    setUsername('');
+    setPassword('');
+    setEmail('');
+    setUser(null);
+  };
 
   return (
     <div className="app">
@@ -120,24 +155,82 @@ function App() {
         </div>
       </Modal>
 
+      <Modal
+        open={openSignIn}
+        onClose={() => { setOpenSignIn(false); }}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signIn}>Sign In</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt=""
         />
+        {user?.displayName ? (
+          <Button onClick={signOut}>Logout</Button>
+        ) : (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
       </div>
 
-      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      <div className="app__posts">
+        <div className="app__postsLeft">
+          {
+            posts.map(({ id, post }) => (
+              <Post key={id} postId={id} user={user} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+            ))
+          }
+        </div>
+        {/* <div className="app__postsRight">
+          <InstagramEmbed
+            url='https://www.instagram.com/p/COeYt7RDNea/'
+            clientAccessToken='123|456'
+            maxWidth={320}
+            hideCaption={false}
+            containerTagName='div'
+            protocol=''
+            injectScript
+            onLoading={() => { }}
+            onSuccess={() => { }}
+            onAfterRender={() => { }}
+            onFailure={() => { }}
+          />
+        </div> */}
+      </div>
 
-      <h1>HELLO Clever Programmers Let's build an Instagram Clone with React!</h1>
-
-      {
-        posts.map(({ id, post }) => (
-          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
-        ))
-      }
-
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ) : (
+        <h3>Sorry you need to Login to upload</h3>
+      )}
     </div>
   );
 }
